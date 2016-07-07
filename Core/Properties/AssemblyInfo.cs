604acf3e2e26e591f063cd113e4f87ac3ba1fa8d
Frank Jiang <fjiang2@hotmail.com>
2016-07-06 21:30:52 -0500

    release version 1.7.1.0

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sys.Core")]
-[assembly: AssemblyDescription("Sys Core library")]
+[assembly: AssemblyDescription("System Core Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Datum Connect")]
 [assembly: AssemblyProduct("System Core")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
 
