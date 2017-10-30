5c63cb6c94e2634d133692088ced604f397a4087
Frank Jiang <fjiang2@hotmail.com>
2017-10-30 01:10:31 -0500

    keep changes

---
+++ Core/Sys/Log.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sys
+{
+    public class Log
+    {
+        private static ReaderWriterLock rwLock = new ReaderWriterLock();
+        private static int lockTimeout = 5 * 1000;
+        private static string path = "sys.log";
+
+        private static void Write(string path, string text)
+        {
+            try
+            {
+                rwLock.AcquireWriterLock(lockTimeout);
+
+                int attempts = 0;
+                StreamWriter writer = null;
+
+                L1:
+                try
+                {
+                    if (attempts < 3)
+                        writer = File.AppendText(path);
+                    else
+                        return;
+                }
+                catch (Exception)
+                {
+                    attempts++;
+                    goto L1;
+                }
+
+
+                if (writer == null)
+                    return;
+
+                try
+                {
+                    writer.WriteLine(text);
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    writer.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                rwLock.ReleaseWriterLock();
+            }
+        }
+
+
+        public static void Error(string text)
+        {
+            text = History(text);
+            Write(path, text);
+        }
+
+        public static void Error(string text, Exception ex)
+        {
+
+        }
+
+        public static void Warn(string text)
+        {
+
+        }
+
+
+        private static string History(string text)
+        {
+            string now = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.ffff");
+            return $"{now} {text}";
+        }
+
+
+
+        private string GetStackInfo(SeverityLevel level)
+        {
+            StringBuilder sb = new StringBuilder();
+            var stackInfo = new StackInfo();
+            sb.Append($"\t-{level}-\t")
+            .Append("T[")
+            .Append(Thread.CurrentThread.GetHashCode().ToString())
+            .Append("]\t")
+            .Append($"{stackInfo}")
+            .Append(": ");
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Core/Sys/StackInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Sys
+{
+    class StackInfo
+    {
+        public string FileName { get; } = string.Empty;
+        public int LineNumber { get; } = 0;
+        public string ObjectName { get; } = "Unknown";
+        public string MethodName { get; } = "Unknown";
+
+        public StackInfo()
+        {
+            StackTrace st = new StackTrace();
+
+            StackFrame sf = new StackFrame(4, true);
+
+            if (sf == null)
+                return;
+
+            try
+            {
+                int p;
+
+                LineNumber = sf.GetFileLineNumber();
+
+                System.Reflection.MethodBase method = sf.GetMethod();
+                if (method != null)
+                {
+                    if (method.ReflectedType != null)
+                        ObjectName = method.ReflectedType.ToString();
+                    else
+                        ObjectName = "global";
+
+                    MethodName = method.ToString();
+                    p = MethodName.IndexOf(" ") + 1;
+                    MethodName = MethodName.Substring(p, MethodName.Length - p);
+                }
+
+                FileName = sf.GetFileName();
+                if (FileName != null)
+                {
+                    p = FileName.LastIndexOf(@"\") + 1;
+                    FileName = FileName.Substring(p, FileName.Length - p);
+                }
+
+            }
+            catch
+            {
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"File:{FileName} line:{LineNumber} {ObjectName}.{MethodName}";
+        }
+    }
+}
