bdde1d409098dc8c0565012b0ed9fd859916ae3d
Frank Jiang <fjiang2@hotmail.com>
2020-04-17 13:33:07 -0500

    Create new class SqlScriptGeneration and keep functionality unchanged

---
+++ syscore/Compare/SqlScriptGeneration.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Data.Common;
+
+namespace Sys.Data.Comparison
+{
+    /// <summary>
+    /// Generate INSERT,
+    /// UPDATE
+    /// and IF EXISTS UPDATE ELSE INSERT
+    /// </summary>
+    class SqlScriptGeneration
+    {
+        private SqlScriptType type;
+        private TableDataClause script;
+        private ITableSchema schema;
+        private int count;
+
+        public Locator Where { get; set; }
+        public bool HasIfExists { get; set; }
+
+        public SqlScriptGeneration(SqlScriptType type, ITableSchema schema)
+        {
+            this.type = type;
+            this.schema = schema;
+            this.script = new TableDataClause(schema);
+        }
+
+        public int Generate(StreamWriter writer)
+        {
+            TableName tableName = schema.TableName;
+            string sql = string.Format("SELECT * FROM {0}", tableName);
+            if (Where != null)
+                sql = string.Format("SELECT * FROM {0} WHERE {1}", tableName, Where);
+
+            SqlCmd cmd = new SqlCmd(tableName.Provider, sql);
+
+            count = 0;
+            cmd.Read(reader => GenerateByDbReader(reader, writer));
+            return count;
+        }
+
+        private int GenerateByDbReader(DbDataReader reader, StreamWriter writer)
+        {
+            DataTable schema1 = reader.GetSchemaTable();
+
+            string[] columns = schema1.AsEnumerable().Select(row => row.Field<string>("ColumnName")).ToArray();
+            object[] values = new object[columns.Length];
+
+            while (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    reader.GetValues(values);
+                    var pairs = new ColumnPairCollection(columns, values);
+                    GenerateRow(writer, pairs);
+
+                    count++;
+                    if (count % 5000 == 0)
+                        writer.WriteLine(TableClause.GO);
+
+                }
+
+                reader.NextResult();
+            }
+
+            if (count != 0)
+                writer.WriteLine(TableClause.GO);
+
+            return count;
+        }
+
+        private void GenerateRow(StreamWriter writer, ColumnPairCollection pairs)
+        {
+            switch (type)
+            {
+                case SqlScriptType.INSERT:
+                    if (HasIfExists)
+                        writer.WriteLine(script.IF_NOT_EXISTS_INSERT(pairs));
+                    else
+                        writer.WriteLine(script.INSERT(pairs));
+                    break;
+
+                case SqlScriptType.UPDATE:
+                    writer.WriteLine(script.UPDATE(pairs));
+                    break;
+
+                case SqlScriptType.INSERT_OR_UPDATE:
+                    writer.WriteLine(script.IF_NOT_EXISTS_INSERT_ELSE_UPDATE(pairs));
+                    break;
+            }
+        }
+    }
+}
