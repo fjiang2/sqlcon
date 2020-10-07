4bce28c61c2cf073479f263869d1238929b094c3
F Jiang <fjiang2@hotmail.com>
2020-10-07 14:34:15 -0500

    Simplify class Query

--- syscore/Data/Linq/Query.cs
+++ syscore/Data/Linq/Query.cs
@@ -9,107 +9,71 @@ namespace Sys.Data.Linq
 {
     public static class Query
     {
-        public static Func<string, IDbCmd> SqlCommand { get; set; } = query => new SqlCmd(query);
+        public static Func<string, IDbCmd> DefaultSqlCommand { get; set; } = query => new SqlCmd(query);
+
+        private static T Invoke<T>(this Func<DataContext, T> func)
+        {
+            using (var db = new DataContext(DefaultSqlCommand))
+            {
+                return func(db);
+            }
+        }
 
         public static IEnumerable<TEntity> Select<TEntity>(string where = null) where TEntity : class
         {
-            using (var db = new DataContext(SqlCommand))
-            {
-                var table = db.GetTable<TEntity>();
-                return table.Select(where);
-            }
+            return Invoke(db => db.GetTable<TEntity>().Select(where));
         }
 
         public static IEnumerable<TEntity> Select<TEntity>(this Expression<Func<TEntity, bool>> where) where TEntity : class
         {
-            using (var db = new DataContext(SqlCommand))
-            {
-                var table = db.GetTable<TEntity>();
-                return table.Select(where);
-            }
+            return Invoke(db => db.GetTable<TEntity>().Select(where));
         }
 
         public static QueryResultReader Select(this Action<DataContext> action)
         {
-            using (var db = new DataContext(SqlCommand))
+            return Invoke(db =>
             {
                 action(db);
                 return db.SumbitQueries();
-            }
-        }
-
-        public static int Submit(this Action<DataContext> action)
-        {
-            using (var db = new DataContext(SqlCommand))
-            {
-                action(db);
-                return db.SubmitChanges();
-            }
+            });
         }
 
         public static int Submit<TEntity>(this Action<Table<TEntity>> action) where TEntity : class
         {
-            using (var db = new DataContext(SqlCommand))
+            return Invoke(db =>
             {
                 var table = db.GetTable<TEntity>();
                 action(table);
                 return db.SubmitChanges();
-            }
+            });
         }
 
         public static int Insert<TEntity>(this IEnumerable<TEntity> entities) where TEntity : class
-        {
-            return Submit<TEntity>(table => table.InsertOnSubmit(entities));
-        }
+            => Submit<TEntity>(table => table.InsertOnSubmit(entities));
 
         public static int Update<TEntity>(this IEnumerable<TEntity> entities) where TEntity : class
-        {
-            return Submit<TEntity>(table => table.UpdateOnSubmit(entities));
-        }
+            => Submit<TEntity>(table => table.UpdateOnSubmit(entities));
 
         public static int PatialUpdate<TEntity>(this IEnumerable<object> entities, bool throwException = false) where TEntity : class
-        {
-            return Submit<TEntity>(table => table.PartialUpdateOnSubmit(entities, throwException));
-        }
+            => Submit<TEntity>(table => table.PartialUpdateOnSubmit(entities, throwException));
 
         public static int InsertOrUpdate<TEntity>(this IEnumerable<TEntity> entities) where TEntity : class
-        {
-            return Submit<TEntity>(table => table.InsertOrUpdateOnSubmit(entities));
-        }
+            => Submit<TEntity>(table => table.InsertOrUpdateOnSubmit(entities));
 
         public static int Delete<TEntity>(this IEnumerable<TEntity> entities) where TEntity : class
-        {
-            return Submit<TEntity>(table => table.DeleteOnSubmit(entities));
-        }
+            => Submit<TEntity>(table => table.DeleteOnSubmit(entities));
 
-        public static IEnumerable<TSubEntity> Expand<TEntity, TSubEntity>(this IEnumerable<TEntity> entities)
-         where TEntity : class
-         where TSubEntity : class
-        {
-            using (var db = new DataContext(SqlCommand))
-            {
-                return db.Expand<TEntity, TSubEntity>(entities);
-            }
-        }
+        public static IEnumerable<TSubEntity> Expand<TEntity, TSubEntity>(this IEnumerable<TEntity> entities) where TEntity : class where TSubEntity : class
+            => Invoke(db => db.Expand<TEntity, TSubEntity>(entities));
 
-        public static QueryResultReader Expand<TEntity>(this IEnumerable<TEntity> entities)
-         where TEntity : class
-        {
-            using (var db = new DataContext(SqlCommand))
-            {
-                return db.Expand(entities);
-            }
-        }
+        public static QueryResultReader Expand<TEntity>(this IEnumerable<TEntity> entities) where TEntity : class
+            => Invoke(db => db.Expand(entities));
 
         public static IEnumerable<T> AsEnumerable<T>(this T item)
-        {
-            return new T[] { item };
-        }
+            => new T[] { item };
 
         public static IEnumerable<T> Enumerable<T>(params T[] items)
-        {
-            return items;
-        }
+            => items;
 
     }
 }
