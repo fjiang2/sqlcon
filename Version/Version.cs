ce05eee7efc56caa31de69147d888e69c5495a56
F Jiang <fjiang@github.com>
2022-03-11 14:00:14 -0600

    fixed bug on ident.cs

--- Version/Version.cs
+++ Version/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.6.2.0")]
-[assembly: AssemblyFileVersion("2.6.2.0")]
+[assembly: AssemblyVersion("2.6.3.1")]
+[assembly: AssemblyFileVersion("2.6.3.1")]
