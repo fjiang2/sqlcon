af4337727926d3bc6961163463c5f242bef77cbf
F Jiang <fjiang@github.com>
2022-03-14 11:49:23 -0500

    Updated version to 2.6.4.0

--- Version/Version.cs
+++ Version/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.6.3.1")]
-[assembly: AssemblyFileVersion("2.6.3.1")]
+[assembly: AssemblyVersion("2.6.4.0")]
+[assembly: AssemblyFileVersion("2.6.4.0")]
